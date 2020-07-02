1d9e0168f310c8a6ef32ae62002612b389f226f4
rangdl <378694192@qq.com>
2020-07-02 11:33:37 +0800

    新增模块spring-rang-tool 工具

---
+++ spring-rang-tool/src/main/java/com/rang/tool/SpringRangToolApplication.java
@@ -0,0 +1,13 @@
+package com.rang.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRangToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRangToolApplication.class, args);
+	}
+
+}
---
+++ spring-rang-tool/src/test/java/com/rang/tool/SpringRangToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rang.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRangToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
